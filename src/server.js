df5ef1ffce5ab604a3149a2b531e405305f68478
Sam Goto <goto@chromium.org>
2026-02-24 12:14:41 -0800

    fix: add urlencoded middleware for PAR requests

--- src/server.js
+++ src/server.js
@@ -50,6 +50,7 @@ app.use((req, res, next) => {
 
 // 3. JSON parser
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const validateClient = async (client_id, redirect_uri) => {
   try {
